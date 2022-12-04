919ea003bc8a48458525778269f78ba316e7e34e
naegonggae <https://github.com/naegonggae/>
2022-12-04 23:04:39 +0900

    빼먹은거 수정

--- src/test/java/com/springboot/jpa/controller/UserRestControllerTest.java
+++ src/test/java/com/springboot/jpa/controller/UserRestControllerTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.junit.jupiter.api.Assertions.*;
-@WebMvcTest
+@WebMvcTest(UserRestController.class)
 class UserRestControllerTest {
 
     @Autowired
