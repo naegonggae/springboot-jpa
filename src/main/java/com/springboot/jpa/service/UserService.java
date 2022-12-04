c21a816a24d6d0b156f0f06be004b406c6d3f9c7
naegonggae <https://github.com/naegonggae/>
2022-12-04 22:33:57 +0900

    add 메소드 사용간에 username 중복체크 기능 추가하기

--- src/main/java/com/springboot/jpa/service/UserService.java
+++ src/main/java/com/springboot/jpa/service/UserService.java
@@ -30,7 +30,15 @@ public class UserService {
     public UserResponse addUser(UserRequest dto) {
         // Dto를 Entity로
         User user = dto.toEntity();
-        User savedUser = userRepository.save(user);
-        return new UserResponse(savedUser.getId(), savedUser.getUsername(), "회원 등록 성공");
+        // 저장하기 전에 Username으로 select 해보기
+        // 있으면 중복되었습니다. 메세지 출력하고 save하지 않음
+        // Talend api Tester로 육안으로 테스트해보기
+        Optional<User> selectedUser = userRepository.findByUsername(dto.getUsername());
+        if (selectedUser.isEmpty()) {
+            User savedUser = userRepository.save(user);
+            return new UserResponse(savedUser.getId(), savedUser.getUsername(), "회원 등록 성공");
+        } else {
+            return new UserResponse(null, dto.getUsername(), "이 User는 이미 존재합니다. 다른 이름을 사용하세요.");
+        }
     }
 }
